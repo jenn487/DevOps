f85b8492329024c059ee4aad61c0c58382f87de5
jenn487 <jenniferdocuments07@gmail.com>
2025-05-15 23:42:44 -0400

    ArchivoParaDev

---
+++ AppPrueba/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace AppPrueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            txtTelefono = new TextBox();
+            txtNombre = new TextBox();
+            txtEdad = new TextBox();
+            txtCodigo = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            menuStrip1 = new MenuStrip();
+            nuevoToolStripMenuItem = new ToolStripMenuItem();
+            registrarToolStripMenuItem = new ToolStripMenuItem();
+            modificarToolStripMenuItem = new ToolStripMenuItem();
+            eliminarToolStripMenuItem = new ToolStripMenuItem();
+            groupBox2 = new GroupBox();
+            txtBuscar = new TextBox();
+            label6 = new Label();
+            groupBox3 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            groupBox1.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(txtTelefono);
+            groupBox1.Controls.Add(txtNombre);
+            groupBox1.Controls.Add(txtEdad);
+            groupBox1.Controls.Add(txtCodigo);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 39);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(776, 150);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Datos";
+            // 
+            // txtTelefono
+            // 
+            txtTelefono.Location = new Point(443, 87);
+            txtTelefono.Name = "txtTelefono";
+            txtTelefono.Size = new Size(200, 31);
+            txtTelefono.TabIndex = 7;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(443, 39);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(283, 31);
+            txtNombre.TabIndex = 6;
+            // 
+            // txtEdad
+            // 
+            txtEdad.Location = new Point(106, 87);
+            txtEdad.Name = "txtEdad";
+            txtEdad.Size = new Size(200, 31);
+            txtEdad.TabIndex = 5;
+            // 
+            // txtCodigo
+            // 
+            txtCodigo.Location = new Point(106, 39);
+            txtCodigo.Name = "txtCodigo";
+            txtCodigo.Size = new Size(200, 31);
+            txtCodigo.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(359, 87);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 25);
+            label3.TabIndex = 3;
+            label3.Text = "Telefono:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(359, 39);
+            label4.Name = "label4";
+            label4.Size = new Size(82, 25);
+            label4.TabIndex = 2;
+            label4.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(40, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Edad:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Codigo:";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { nuevoToolStripMenuItem, registrarToolStripMenuItem, modificarToolStripMenuItem, eliminarToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 33);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            nuevoToolStripMenuItem.Size = new Size(80, 29);
+            nuevoToolStripMenuItem.Text = "Nuevo";
+            // 
+            // registrarToolStripMenuItem
+            // 
+            registrarToolStripMenuItem.Name = "registrarToolStripMenuItem";
+            registrarToolStripMenuItem.Size = new Size(97, 29);
+            registrarToolStripMenuItem.Text = "Registrar";
+            // 
+            // modificarToolStripMenuItem
+            // 
+            modificarToolStripMenuItem.Name = "modificarToolStripMenuItem";
+            modificarToolStripMenuItem.Size = new Size(103, 29);
+            modificarToolStripMenuItem.Text = "Modificar";
+            // 
+            // eliminarToolStripMenuItem
+            // 
+            eliminarToolStripMenuItem.Name = "eliminarToolStripMenuItem";
+            eliminarToolStripMenuItem.Size = new Size(90, 29);
+            eliminarToolStripMenuItem.Text = "Eliminar";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(txtBuscar);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Location = new Point(12, 195);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(776, 109);
+            groupBox2.TabIndex = 8;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Buscar";
+            // 
+            // txtBuscar
+            // 
+            txtBuscar.Location = new Point(126, 48);
+            txtBuscar.Name = "txtBuscar";
+            txtBuscar.Size = new Size(600, 31);
+            txtBuscar.TabIndex = 6;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(42, 48);
+            label6.Name = "label6";
+            label6.Size = new Size(82, 25);
+            label6.TabIndex = 2;
+            label6.Text = "Nombre:";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(dataGridView1);
+            groupBox3.Location = new Point(12, 331);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(776, 237);
+            groupBox3.TabIndex = 9;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Listar";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(-1, 30);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(776, 201);
+            dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 589);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem nuevoToolStripMenuItem;
+        private ToolStripMenuItem registrarToolStripMenuItem;
+        private ToolStripMenuItem modificarToolStripMenuItem;
+        private ToolStripMenuItem eliminarToolStripMenuItem;
+        private TextBox txtTelefono;
+        private TextBox txtNombre;
+        private TextBox txtEdad;
+        private TextBox txtCodigo;
+        private Label label3;
+        private Label label4;
+        private Label label2;
+        private Label label1;
+        private GroupBox groupBox2;
+        private TextBox txtBuscar;
+        private Label label6;
+        private GroupBox groupBox3;
+        private DataGridView dataGridView1;
+    }
+}
